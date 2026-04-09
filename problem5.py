3c02a858e26e6ba20c4d65ce408ab6d0354773cb
nhk1720 <159052757+nhk1720@users.noreply.github.com>
2026-04-09 16:47:42 +0000

    add pg

--- problem5.py
+++ problem5.py
@@ -1,2 +1,3 @@
 a=int(input("Enter number 1: " ))
 b=int(input("Enter number 2: " ))
+print("Avg of two numbers ",(a+b)/2)
---
+++ problem6.py
@@ -0,0 +1,4 @@
+a=int(input("Enter your number: "))
+print("the square of the number is ",a**2)
+print("a=",a)
+print("the square of the number is ",a*a)
