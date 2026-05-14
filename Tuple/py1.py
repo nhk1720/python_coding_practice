f32a93814cd2bb91aba9f95c08ac1f7238d80f04
nhk1720 <159052757+nhk1720@users.noreply.github.com>
2026-05-14 04:58:56 +0000

    add

---
+++ Tuple/py1.py
@@ -0,0 +1,18 @@
+t1=()
+print(type(t1))
+t2=(10,10,50,20,50,70)
+# print(t2)
+# print(type(t2))
+print(t2.count(50))
+
+value=t2.index(10)
+print(value)
+print(len(t2))
+
+value=t2[-6:-1]
+print(value)
+a,b,c,d,e,f=t2
+# unpacking
+print(t2)
+print(a)
+print(c)
---
+++ list/chapter4.py
@@ -0,0 +1,10 @@
+# fruits= input("Enter fruits name: ").split()
+# print(fruits)
+
+
+f=[]
+
+for i in range(7):
+    fruits=input("Enter any fruit name:")
+    f.append(fruits)
+print(f)
