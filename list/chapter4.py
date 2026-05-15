074d17f0d7ae571f6b8cbec0d958a99a257204dc
nhk1720 <159052757+nhk1720@users.noreply.github.com>
2026-05-15 17:16:37 +0000

    complete chap 4

--- list/chapter4.py
+++ list/chapter4.py
@@ -1,10 +1,49 @@
-# fruits= input("Enter fruits name: ").split()
-# print(fruits)
+#  1st program
+
+# Write a program to store seven fruits in a list entered by the user.
+# f=[]
+
+# for i in range(7):
+#     fruits=input("Enter any fruit name:")
+#     f.append(fruits)
+# print(f)
 
 
-f=[]
+# 2nd program================
+# Write a program to accept marks of 6 students and display them in a sorted
+# manner.
+# num=[]
 
-for i in range(7):
-    fruits=input("Enter any fruit name:")
-    f.append(fruits)
-print(f)
+# for i in range(6):
+#     n=int(input("Enter any number: "))
+#     num.append(n)
+# num.sort()
+# print(num)
+
+# 3rd============
+
+# Check that a tuple type cannot be changed in python.
+
+# tup=(34,"Nikhil",90,20)
+# print(type(tup))
+# print(tup.count("Nikhil"))
+# tup[0]=3
+# print(tup)
+
+# Write a program to sum a list with 4 numbers.
+
+# num=[]
+# n1=0
+# for i in range(4):
+#     n=int(input("Enter any number: "))
+
+#     num.append(n)
+#     n1=n1+n
+
+# print("Sun of number: ",n1)
+
+# Write a program to count the number of zeros in the following tuple:
+a = (7, 0, 8, 0, 0, 9)
+print("Count of number ",a.count(0))
+
+
